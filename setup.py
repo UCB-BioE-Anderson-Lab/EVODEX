a182c099c053b1fba6cab41f5dd66e4bead8a7ae
J. Christopher Anderson <jcanderson2167@gmail.com>
2024-07-17 16:27:20 -0700

    Cleanup of data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evodex',
-    version='0.1.15',
+    version='0.1.16',
     packages=find_packages(include=['evodex', 'evodex.*']),
     install_requires=[
         'rdkit-pypi',
@@ -10,6 +10,20 @@ setup(
         'numpy',
     ],
     include_package_data=True,
+    package_data={
+        'evodex': [
+            'data/EVODEX-C_reaction_operators.csv',
+            'data/EVODEX-Cm_reaction_operators.csv',
+            'data/EVODEX-E_reaction_operators.csv',
+            'data/EVODEX-E_synthesis_subset.csv',
+            'data/EVODEX-Em_reaction_operators.csv',
+            'data/EVODEX-F_unique_formulas.csv',
+            'data/EVODEX-M_mass_spec_subset.csv',
+            'data/EVODEX-M_unique_masses.csv',
+            'data/EVODEX-N_reaction_operators.csv',
+            'data/EVODEX-Nm_reaction_operators.csv',
+        ],
+    },
     author='J. Christopher Anderson',
     author_email='jcanderson@berkeley.edu',
     description='A project to process enzymatic reactions',
