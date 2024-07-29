136f5541046c7aa8b65d86b04e683bee1ca31f78
J. Christopher Anderson <jcanderson2167@gmail.com>
2024-07-29 11:47:03 -0700

    Generalized synthesis code to allow version numbers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evodex',
-    version='0.1.19',
+    version='0.1.21',
     packages=find_packages(include=['evodex', 'evodex.*']),
     install_requires=[
         'rdkit-pypi',
