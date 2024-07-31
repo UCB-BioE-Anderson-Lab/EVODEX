029ac45d7cf35dcd438b3995a23f079ba68ebc10
J. Christopher Anderson <jcanderson2167@gmail.com>
2024-07-31 12:55:59 -0700

    Added working notebooks

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evodex',
-    version='0.1.24',
+    version='0.1.25',
     packages=find_packages(include=['evodex', 'evodex.*']),
     install_requires=[
         'rdkit-pypi',
