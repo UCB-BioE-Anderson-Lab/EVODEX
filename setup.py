f5cc92757fd2f88c5508374212cc24c00105e14a
J. Christopher Anderson <jcanderson2167@gmail.com>
2024-08-03 14:57:34 -0700

    minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evodex',
-    version='0.1.25',
+    version='0.1.26',
     packages=find_packages(include=['evodex', 'evodex.*']),
     install_requires=[
         'rdkit-pypi',
