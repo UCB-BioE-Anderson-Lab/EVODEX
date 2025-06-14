2b56c03d4e517ed72ed217cbc366ebd31b855128
J. Christopher Anderson <jcanderson2167@gmail.com>
2025-06-14 14:38:57 -0700

    1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evodex',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(include=['evodex', 'evodex.*']),
     install_requires=[
         'rdkit-pypi',
