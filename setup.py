9c66a8f190595ae93a0e86b91296d66de02a65eb
J. Christopher Anderson <jcanderson2167@gmail.com>
2025-12-15 10:24:35 -0800

    fixed pypi, updated synthesis

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="evodex",
-    version="2.0.0",
+    version="2.0.1",
     packages=find_packages(include=["evodex", "evodex.*"]),
     install_requires=[
-        "rdkit-pypi",
+        "rdkit",
         "pandas",
         "numpy",
     ],
